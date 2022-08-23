a06bb6ef0ec4f88541bb8ad4fde5189b9710e47a
Pablo Collado Soto <pcolladosoto@gmx.com>
2022-08-23 21:55:53 +0200

    Initial Commit

---
+++ sampleProj/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  sampleProj
+//
+//  Created by Pablo Collado Soto on 23/8/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
